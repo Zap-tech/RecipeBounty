ec0df0b440f258a698097e6bf61ae72bb8452968
Benjamin Zaporzan <ben@district0x.io>
2018-09-24 09:46:19 -0400

    Refactoring: housekeeping, reorganized and added shared folders

---
+++ dev/shared/rbounty/dev/shared.cljs
@@ -0,0 +1,2 @@
+(ns rbounty.dev.shared
+  "Directory contains shared code between browser ui and node server.")
---
+++ test/shared/rbounty/shared_test.cljs
@@ -0,0 +1,5 @@
+(ns rbounty.shared-test
+  "Folder contains shared tests between browser ui and node server."
+  (:require [clojure.test :refer [deftest is are testing]]))
+
+
