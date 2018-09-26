0b38f6096178ce476d2ebcda3fe3643656436040
Benjamin Zaporzan <ben@district0x.io>
2018-09-26 01:12:02 -0400

    Added development environment for browser

--- dev/ui/cljs/user.cljs
+++ dev/ui/cljs/user.cljs
@@ -1,5 +1,54 @@
 (ns cljs.user
-  "Clojurescript Entrypoint for Browser UI Repl.")
+  "Clojurescript Entrypoint for Browser UI Repl."
+  (:require
+   [mount.core :as mount]
 
-(println "Hello From UI!")
+   ;; Mount State Components
+   [district.ui.reagent-render]))
 
+
+(def help-msg "
+CLJS Client Repl Commands:
+
+  (start)          ;; Start Mount Components.
+  (stop)           ;; Stop Mount Components.
+  (restart)        ;; Restart Mount Components [useful].
+
+  (open-tests)     ;; Opens a separate window containing browser tests.
+
+  (help)           ;; Display this help message.
+")
+
+(defn start
+  "Reloaded workflow, start state components."
+  []  
+  (mount/start))
+
+
+(defn stop
+  "Reloaded workflow, stop the state components."
+  []
+  (mount/stop))
+
+
+(defn restart
+  "Reloaded workflow, restart the state components."
+  []
+  (stop)
+  (start))
+
+
+(defn open-tests
+  "Opens the browser test runner."
+  []
+  (let [host (aget js/window "location" "host")]
+    (.open js/window (str "http://" host "/test-runner.html") "_blank")))
+
+
+(defn help
+  "Print Help Message."
+  []
+  (println help-msg))
+
+
+(defonce init-msg (help))
